e5f75e4fa517627d36690f653042eac4b93d7477
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-22 13:44:43 +0100

    Adding Stylize

--- src/frameworks.ts
+++ src/frameworks.ts
@@ -5,6 +5,7 @@ const frameworks: Record<string, {
   "attri": {name: "AttriCSS", htmlUrl: "/attri.html", cssUrl: "/styles/attri.css" , siteUrl: "https://raj457036.github.io/attriCSS/"},
   "mvp": { name: "MVP.css", htmlUrl: "/mvp.html", cssUrl: "https://unpkg.com/mvp.css", siteUrl: "https://andybrewer.github.io/mvp/" },
   "tufte": { name: "Tufte.css", htmlUrl: "/tufte.html", cssUrl: "/styles/tufte.css", siteUrl: "https://github.com/edwardtufte/tufte-css" },
+  "stylize": { name: "Stylize", htmlUrl: "/stylize.html", cssUrl: "/styles/stylize.css", siteUrl: "https://vasanthv.com/stylize.css/demo.html" },
   "water": { name: "Water.css", htmlUrl: "/water.html", cssUrl: new URL("https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"), siteUrl: "https://watercss.kognise.dev/" }
 }
 
