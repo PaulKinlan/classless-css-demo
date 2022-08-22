f2b712e170224453a708c0077a7bd2d4f1bde965
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-22 11:33:36 +0100

    window location

--- main.ts
+++ main.ts
@@ -34,7 +34,7 @@ class StaticFileHandler {
 const frameworks: Record<string, {
   name: string, htmlUrl: string, cssUrl: URL
 }> = {
-  "": { name: "Browser", htmllUrl: "/", cssUrl: new URL("/styles/index.css", window.location) }, // This is a hack to use my default style
+  "": { name: "Browser", htmllUrl: "/", cssUrl: new URL(`${window.location}/styles/index.css`) }, // This is a hack to use my default style
   "water": { name: "Water.css", htmlUrl: "/water.html", cssUrl: new URL("https://cdn.jsdelivr.net/npm/water.css@2/out/water.css") }
 }
 
