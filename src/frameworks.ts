3374c625e13c8ada1f2153119faa1f873ee63fbd
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-22 13:37:45 +0100

    Adding Attri

--- src/frameworks.ts
+++ src/frameworks.ts
@@ -2,6 +2,7 @@ const frameworks: Record<string, {
   name: string, htmlUrl: string, cssUrl: URL
 }> = {
   "": { name: "Browser default style", htmlUrl: "/", cssUrl: `/styles/index.css`, siteUrl: "https://meiert.com/en/blog/user-agent-style-sheets/" }, // This is a hack to use my default style
+  "attri": {name: "AttriCSS", htmlUrl: "/attri.html", cssUrl: "/styles/attri.css" , siteUrl: "https://raj457036.github.io/attriCSS/"}
   "mvp": { name: "MVP.css", htmlUrl: "/mvp.html", cssUrl: "https://unpkg.com/mvp.css", siteUrl: "https://andybrewer.github.io/mvp/" },
   "tufte": { name: "Tufte.css", htmlUrl: "/tufte.html", cssUrl: "/styles/tufte.css", siteUrl: "https://github.com/edwardtufte/tufte-css" },
   "water": { name: "Water.css", htmlUrl: "/water.html", cssUrl: new URL("https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"), siteUrl: "https://watercss.kognise.dev/" }
