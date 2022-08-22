af089a0b80a382cf85da433efda8aa9d788d2b44
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-22 11:50:02 +0100

    mvp name

--- src/frameworks.ts
+++ src/frameworks.ts
@@ -2,7 +2,7 @@ const frameworks: Record<string, {
     name: string, htmlUrl: string, cssUrl: URL
   }> = {
     "": { name: "Browser", htmlUrl: "/", cssUrl: `/styles/index.css` }, // This is a hack to use my default style
-    "mvp": {name: "MVP.css", htmlUrl: "/mvp.css", cssUrl: 'https://unpkg.com/mvp.css'},
+    "mvp": {name: "MVP.css", htmlUrl: "/mvp.html", cssUrl: 'https://unpkg.com/mvp.css'},
     "water": { name: "Water.css", htmlUrl: "/water.html", cssUrl: new URL("https://cdn.jsdelivr.net/npm/water.css@2/out/water.css") },
     "tufte": { name: "Tufte.css", htmlUrl: "/tufte.html", cssUrl: '/styles/tufte.css' }
   }
