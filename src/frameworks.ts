27efaaa4298edf12ce7285098c7f65cc90ef6903
Paul Kinlan <paul.kinlan@gmail.com>
2022-08-22 12:01:04 +0100

    Alphabetize

--- src/frameworks.ts
+++ src/frameworks.ts
@@ -3,8 +3,8 @@ const frameworks: Record<string, {
 }> = {
   "": { name: "Browser", htmlUrl: "/", cssUrl: `/styles/index.css`, siteUrl: "https://meiert.com/en/blog/user-agent-style-sheets/" }, // This is a hack to use my default style
   "mvp": { name: "MVP.css", htmlUrl: "/mvp.html", cssUrl: "https://unpkg.com/mvp.css", siteUrl: "https://andybrewer.github.io/mvp/" },
-  "water": { name: "Water.css", htmlUrl: "/water.html", cssUrl: new URL("https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"), siteUrl: "https://watercss.kognise.dev/" },
-  "tufte": { name: "Tufte.css", htmlUrl: "/tufte.html", cssUrl: "/styles/tufte.css", siteUrl: "https://github.com/edwardtufte/tufte-css" }
+  "tufte": { name: "Tufte.css", htmlUrl: "/tufte.html", cssUrl: "/styles/tufte.css", siteUrl: "https://github.com/edwardtufte/tufte-css" },
+  "water": { name: "Water.css", htmlUrl: "/water.html", cssUrl: new URL("https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"), siteUrl: "https://watercss.kognise.dev/" }
 }
 
 export default frameworks;
